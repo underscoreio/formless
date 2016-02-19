bb0997b774912744cd4263bea90aa092407c3ad2
Noel Welsh <noel@untyped.com>
2016-02-19 22:42:51 +0000

    Extend the model to control text rendering; add append
    We could lift answers into the list monoid but it's really not worth it
    in this case IMHO

--- core/src/main/scala/formless/data/base.scala
+++ core/src/main/scala/formless/data/base.scala
@@ -4,17 +4,24 @@ package data
 final case class Title(get: String) extends AnyVal
 final case class Description(get: String) extends AnyVal
 
-final case class Form(title: Title, description: Description, fields: List[Field])
+final case class Form(title: Title, description: Description, fields: List[Field]) {
+  def append(field: Field): Form =
+    this.copy(fields = fields :+ field)
+}
 
 final case class Field(title: Title, description: Option[Description], answer: Option[Answer])
 
 sealed abstract class Answer
-final case class Text(contents: String) extends Answer
+final case class Text(contents: String, rendering: TextRendering) extends Answer
 sealed abstract class MultipleChoice extends Answer {
   def options: List[String]
 }
 final case class ChooseOne(options: List[String], rendering: SingleChoiceRendering) extends MultipleChoice
 final case class ChooseMany(options: List[String]) extends MultipleChoice
+
+sealed trait TextRendering
+final case object TextLine extends TextRendering
+final case object TextBox extends TextRendering
 
 sealed trait SingleChoiceRendering
 final case object Dropdown extends SingleChoiceRendering
